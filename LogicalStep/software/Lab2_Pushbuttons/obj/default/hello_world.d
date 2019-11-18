1754503d507fdb6baa15227bd0eeeb6a7a283655
Sohini Mukherjee <s23mukhe@uwaterloo.ca>
2019-11-17 22:12:06 -0500

    Lab4 phase 3 and part of 4

---
+++ LogicalStep/software/Lab2_Pushbuttons/obj/default/hello_world.d
@@ -0,0 +1,33 @@
+obj/default/hello_world.o: hello_world.c \
+ ../Lab2_Pushbuttons_bsp//HAL/inc/alt_types.h \
+ ../Lab2_Pushbuttons_bsp//drivers/inc/altera_avalon_timer.h \
+ ../Lab2_Pushbuttons_bsp//HAL/inc/sys/alt_dev.h \
+ ../Lab2_Pushbuttons_bsp/system.h ../Lab2_Pushbuttons_bsp/linker.h \
+ ../Lab2_Pushbuttons_bsp//HAL/inc/sys/alt_llist.h \
+ ../Lab2_Pushbuttons_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../Lab2_Pushbuttons_bsp//HAL/inc/sys/alt_warning.h \
+ ../Lab2_Pushbuttons_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../Lab2_Pushbuttons_bsp//HAL/inc/io.h \
+ ../Lab2_Pushbuttons_bsp//HAL/inc/alt_types.h
+
+../Lab2_Pushbuttons_bsp//HAL/inc/alt_types.h:
+
+../Lab2_Pushbuttons_bsp//drivers/inc/altera_avalon_timer.h:
+
+../Lab2_Pushbuttons_bsp//HAL/inc/sys/alt_dev.h:
+
+../Lab2_Pushbuttons_bsp/system.h:
+
+../Lab2_Pushbuttons_bsp/linker.h:
+
+../Lab2_Pushbuttons_bsp//HAL/inc/sys/alt_llist.h:
+
+../Lab2_Pushbuttons_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../Lab2_Pushbuttons_bsp//HAL/inc/sys/alt_warning.h:
+
+../Lab2_Pushbuttons_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../Lab2_Pushbuttons_bsp//HAL/inc/io.h:
+
+../Lab2_Pushbuttons_bsp//HAL/inc/alt_types.h:
